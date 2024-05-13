2bb7f1429cb880f503efdbd2520a7aab845ce06a
Maria <msinukova@gmail.com>
2024-05-13 17:34:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("App for measure converting. ");
+        System.out.println("Version 1.0.");
+        double kgs = 5;
+        double pnds = 12;
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is " + pounds + " pounds. " + kilos + " kgs.");
+    }
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
+    }
+}
+
